6fa6221e70c450e57520430dea3248f47f716fb4
huychang2012 <huychang2012@outlook.com>
2017-06-03 00:36:46 +0700

    List Objetc Kotlin

--- app/src/main/java/com/collagemedia/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/collagemedia/kotlinexample/MainActivity.kt
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import com.collagemedia.kotlinexample.adapter.ListStudentAdapter
 import com.collagemedia.kotlinexample.model.StudentModel
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.toast
+import java.util.*
 import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
     val url = "https://avatars0.githubusercontent.com/u/1446536?v=3&s=200"
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,17 +21,45 @@ class MainActivity : AppCompatActivity() {
         tvMain.text = name
 
         val lstStudent: ArrayList<StudentModel> = ArrayList()
-        lstStudent.add(StudentModel(510111, url, "John", "26/08/1992"))
-        lstStudent.add(StudentModel(510111, url, "Dept", "01/01/1994"))
-        lstStudent.add(StudentModel(510111, url, "Jana", "09/03/1992"))
-        lstStudent.add(StudentModel(510111, url, "Temo", "15/05/1991"))
-        lstStudent.add(StudentModel(510111, url, "Catilyn", "09/12/1997"))
-        lstStudent.add(StudentModel(510111, url, "Garen", "21/04/1990"))
-        lstStudent.add(StudentModel(510111, url, "Yasuo", "16/01/1987"))
-        lstStudent.add(StudentModel(510111, url, "Mundo", "30/12/1988"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "John", "26/08/1992"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Dept", "01/01/1994"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Jana", "09/03/1992"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Temo", "15/05/1991"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Catilyn", "09/12/1997"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Garen", "21/04/1990"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Yasuo", "16/01/1987"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Mundo", "30/12/1988"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "John", "26/08/1992"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Dept", "01/01/1994"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Jana", "09/03/1992"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Temo", "15/05/1991"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Catilyn", "09/12/1997"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Garen", "21/04/1990"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Yasuo", "16/01/1987"))
+        lstStudent.add(StudentModel(510111, radomUrl(initUrl()), "Mundo", "30/12/1988"))
 
-        lvMain.adapter = ListStudentAdapter(this@MainActivity, lstStudent)
+        lvMain.adapter = ListStudentAdapter(this, lstStudent)
+        lvMain.setOnItemClickListener { parent, view, position, id ->
+            toast(lstStudent[position].name)
+        }
 
+        lvMain.setOnItemLongClickListener { parent, view, position, id ->
+            toast(lstStudent[position].birthday)
+            false
+        }
+    }
 
+    fun radomUrl(array: ArrayList<String>): String {
+        val radom: Random = Random()
+        val pos = radom.nextInt(array.size)
+        return array[pos]
+    }
+
+    fun initUrl() : ArrayList<String>{
+        val datas : ArrayList<String> = ArrayList()
+        datas.add("https://raw.githubusercontent.com/twitter/twemoji/gh-pages/72x72/1f3b2.png")
+        datas.add("https://raw.githubusercontent.com/twitter/twemoji/gh-pages/72x72/1f3bb.png")
+        datas.add("https://raw.githubusercontent.com/twitter/twemoji/gh-pages/72x72/1f3e0.png")
+        return datas
     }
 }
