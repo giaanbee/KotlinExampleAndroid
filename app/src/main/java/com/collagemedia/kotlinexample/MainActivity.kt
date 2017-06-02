7e6ecc3e1e50f26e907415d9d4f7ae16c5d19f0b
huychang2012 <huychang2012@outlook.com>
2017-06-02 17:36:17 +0700

    add kotlin-android-extensions

--- app/src/main/java/com/collagemedia/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/collagemedia/kotlinexample/MainActivity.kt
@@ -2,11 +2,14 @@ package com.collagemedia.kotlinexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvMain.setText("Chán ơi là chán")
     }
 }
