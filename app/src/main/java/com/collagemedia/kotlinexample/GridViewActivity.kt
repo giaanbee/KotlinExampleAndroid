0b05ace1698ccdb2e2da4ad2d5ca0065b9b1cbff
Nguyễn Huy <giaanbee@outlook.com>
2017-06-03 20:33:13 +0700

    update data

--- app/src/main/java/com/collagemedia/kotlinexample/GridViewActivity.kt
+++ app/src/main/java/com/collagemedia/kotlinexample/GridViewActivity.kt
@@ -31,26 +31,9 @@ class GridViewActivity : AppCompatActivity(), AnkoLogger {
         initView()
     }
 
-    fun initData(): ArrayList<StudentModel> {
-        val data: ArrayList<StudentModel> = ArrayList()
-        data.add(StudentModel(1, R.mipmap.avatar1, "John", "26/08/1992"))
-        data.add(StudentModel(2, R.mipmap.avatar2, "Dept", "01/01/1994"))
-        data.add(StudentModel(3, R.mipmap.avatar3, "Jana", "09/03/1992"))
-        data.add(StudentModel(4, R.mipmap.avatar4, "Temo", "15/05/1991"))
-        data.add(StudentModel(5, R.mipmap.avatar5, "Catilyn", "09/12/1997"))
-        data.add(StudentModel(6, R.mipmap.avatar6, "Garen", "21/04/1990"))
-        data.add(StudentModel(7, R.mipmap.avatar7, "Yasuo", "16/01/1987"))
-        data.add(StudentModel(8, R.mipmap.avatar8, "Mundo", "30/12/1988"))
-        data.add(StudentModel(9, R.mipmap.avatar9, "John", "26/08/1992"))
-        data.add(StudentModel(10, R.mipmap.avatar10, "Dept", "01/01/1994"))
-        data.add(StudentModel(11, R.mipmap.avatar11, "Jana", "09/03/1992"))
-        data.add(StudentModel(12, R.mipmap.avatar12, "Temo", "15/05/1991"))
-        return data
-    }
-
     fun initView(): Unit {
 
-        lstPhoto = initData()
+        lstPhoto = Config.initData()
         val col = 3
         val pH = Config.SCREENWIDTH / col - (dpToPx(5) * 2);
         var layoutManager: RecyclerView.LayoutManager = GridLayoutManager(this, col)
--- app/src/main/java/com/collagemedia/kotlinexample/ListViewActivity.kt
+++ app/src/main/java/com/collagemedia/kotlinexample/ListViewActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.collagemedia.kotlinexample.adapter.ListStudentAdapter
 import com.collagemedia.kotlinexample.model.StudentModel
+import com.collagemedia.kotlinexample.util.Config
 import kotlinx.android.synthetic.main.activity_listview.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.toast
@@ -14,7 +15,7 @@ class ListViewActivity : AppCompatActivity(), AnkoLogger {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listview)
 
-        val lstStudent: ArrayList<StudentModel> = initData()
+        val lstStudent: ArrayList<StudentModel> = Config.initData()
 
         lvMain.adapter = ListStudentAdapter(this, R.layout.item_listview, lstStudent)
         lvMain.setOnItemClickListener { parent, view, position, id ->
@@ -25,22 +26,5 @@ class ListViewActivity : AppCompatActivity(), AnkoLogger {
             toast(lstStudent[position].birthday)
             false
         }
-    }
-
-    fun initData(): ArrayList<StudentModel> {
-        val data: ArrayList<StudentModel> = ArrayList()
-        data.add(StudentModel(1, R.mipmap.avatar1, "John", "26/08/1992"))
-        data.add(StudentModel(2, R.mipmap.avatar2, "Dept", "01/01/1994"))
-        data.add(StudentModel(3, R.mipmap.avatar3, "Jana", "09/03/1992"))
-        data.add(StudentModel(4, R.mipmap.avatar4, "Temo", "15/05/1991"))
-        data.add(StudentModel(5, R.mipmap.avatar5, "Catilyn", "09/12/1997"))
-        data.add(StudentModel(6, R.mipmap.avatar6, "Garen", "21/04/1990"))
-        data.add(StudentModel(7, R.mipmap.avatar7, "Yasuo", "16/01/1987"))
-        data.add(StudentModel(8, R.mipmap.avatar8, "Mundo", "30/12/1988"))
-        data.add(StudentModel(9, R.mipmap.avatar9, "John", "26/08/1992"))
-        data.add(StudentModel(10, R.mipmap.avatar10, "Dept", "01/01/1994"))
-        data.add(StudentModel(11, R.mipmap.avatar11, "Jana", "09/03/1992"))
-        data.add(StudentModel(12, R.mipmap.avatar12, "Temo", "15/05/1991"))
-        return data
     }
 }
