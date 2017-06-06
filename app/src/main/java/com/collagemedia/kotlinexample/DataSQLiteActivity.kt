bee0fdd32b4b0ec3e652478fd15da9421dc4fdba
Nguyễn Huy <giaanbee@outlook.com>
2017-06-07 01:06:24 +0700

    update SQLite Kotlin App

--- app/src/main/java/com/collagemedia/kotlinexample/DataSQLiteActivity.kt
+++ app/src/main/java/com/collagemedia/kotlinexample/DataSQLiteActivity.kt
@@ -2,16 +2,15 @@ package com.collagemedia.kotlinexample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import com.collagemedia.kotlinexample.adapter.ListViewAdapter
 import com.collagemedia.kotlinexample.model.StudentModel
 import com.collagemedia.kotlinexample.sqlite.DataOpenHelper
 import com.collagemedia.kotlinexample.util.Config
 import kotlinx.android.synthetic.main.activity_data_sqlite.*
-import org.jetbrains.anko.AnkoLogger
+import kotlinx.coroutines.experimental.Deferred
+import org.jetbrains.anko.*
+import org.jetbrains.anko.coroutines.experimental.bg
 import org.jetbrains.anko.custom.async
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.uiThread
 
 class DataSQLiteActivity : AppCompatActivity(), AnkoLogger {
     var dataSQL: DataOpenHelper? = null
@@ -24,27 +23,53 @@ class DataSQLiteActivity : AppCompatActivity(), AnkoLogger {
 
         dataSQL = DataOpenHelper.getInstance(this)
 
-        loadData()
+//        loadData()
 
+        initView()
     }
 
+
+    /*--------Using Anko Coroutines--------*/
+    fun getData(): ArrayList<StudentModel> {
+        var data: ArrayList<StudentModel> = ArrayList()
+        if (dataSQL!!.getCount() == 0) {
+            for (item in Config.initData())
+                dataSQL!!.addData(item)
+        }
+        data = dataSQL!!.getAllData()
+        return data
+    }
+
+    fun initView() {
+        kotlinx.coroutines.experimental.async(kotlinx.coroutines.experimental.android.UI) {
+            val data: Deferred<ArrayList<StudentModel>> = bg {
+                // Cha
+                getData()
+            }
+
+            // This code is executed on the UI thread
+            showData(data.await())
+        }
+    }
+
+
+    /*-------Normal--------*/
     private fun loadData() {
         async {
-            if (dataSQL!!.getCount() == 0){
+            if (dataSQL!!.getCount() == 0) {
                 for (item in Config.initData())
                     dataSQL!!.addData(item)
             }
             list = dataSQL!!.getAllData()
 
             uiThread {
-                loadComplete()
+                showData(list)
             }
         }
     }
 
-    private fun loadComplete() {
-        tvLoadComplete.visibility = View.GONE
-        lvData.adapter = ListViewAdapter(this, R.layout.item_listview, list)
+    private fun showData(data: ArrayList<StudentModel>) {
+        lvData.adapter = ListViewAdapter(this, R.layout.item_listview, data)
         lvData.setOnItemClickListener { _, _, position, _ ->
             startActivity(intentFor<ViewPagerActivity>("pos" to position))
         }
