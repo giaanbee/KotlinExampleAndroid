cdede0574bdd03fe797fa60434fb783580349a2a
Nguyễn Huy <giaanbee@outlook.com>
2017-06-10 17:19:26 +0700

    update handler and progress style drawable

--- app/src/main/java/com/collagemedia/kotlinexample/activity/ConstrainLayoutActivity.kt
+++ app/src/main/java/com/collagemedia/kotlinexample/activity/ConstrainLayoutActivity.kt
@@ -1,10 +1,12 @@
 package com.collagemedia.kotlinexample.activity
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import com.collagemedia.kotlinexample.R
 import kotlinx.android.synthetic.main.activity_constrain_layout.*
 import org.jetbrains.anko.toast
+
 
 class ConstrainLayoutActivity : AppCompatActivity() {
 
@@ -12,13 +14,30 @@ class ConstrainLayoutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constrain_layout)
 
-        btnLogin.setOnClickListener {
+        btnStart.setOnClickListener {
             if (checkLogin()) {
                 toast("Login success")
             } else {
                 toast("Login fail")
             }
         }
+
+        val handle = Handler()
+        handle.postDelayed(object : Runnable {
+            override fun run() {
+                val max = progressbar.max
+                progressbar.progress = progressbar.progress + 1
+                progressbar.secondaryProgress = progressbar.secondaryProgress + 2
+                if (progressbar.progress == max) {
+                    progressbar.secondaryProgress = 0
+                    progressbar.progress = 0
+                }
+
+
+                handle.postDelayed(this, 100)
+            }
+        }, 100)
+
     }
 
     fun checkLogin(): Boolean {
